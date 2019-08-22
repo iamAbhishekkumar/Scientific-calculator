a9e790b968acfb6ab3eae45283c3bf59744bb876
abhishekkumar321 <53290881+abhishekkumar321@users.noreply.github.com>
2019-08-22 19:24:12 +0530

    Update Sci_calculator.py

--- Sci_calculator.py
+++ Sci_calculator.py
@@ -3,6 +3,7 @@ import math
 import unicodedata
 
 expression= ""
+
 PI = unicodedata.lookup("GREEK CAPITAL LETTER PI")
 def btn_clear():
     global expression
@@ -54,45 +55,45 @@ frame6.pack(expand= True, fill="both")
 entry_box = Entry(frame1, justify =RIGHT, font="Verdana 20", textvariable = display, bg="WHITE").pack(expand = True, fill="both")
 
 
-btn7 = Button(frame2, text="7", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(7)).pack(side=LEFT, expand=True, fill="both")
-btn8 = Button(frame2, text="8", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(8)).pack(side=LEFT, expand=True, fill="both")
-btn9 = Button(frame2, text="9", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(9)).pack(side=LEFT, expand=True, fill="both")
-btn_mul = Button(frame2, text="*", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('*')).pack(side=LEFT, expand=True, fill="both")
-btn_sin = Button(frame2, text="sin", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('sin(')).pack(side=LEFT, expand=True, fill="both")
-btn_log = Button(frame2, text="log", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('log(')).pack(side=LEFT, expand=True, fill="both")
+btn7 = Button(frame2, text="7", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(7), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn8 = Button(frame2, text="8", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(8), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn9 = Button(frame2, text="9", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(9), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_mul = Button(frame2, text="*", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('*'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_sin = Button(frame2, text="sin", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('sin('), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_log = Button(frame2, text="log", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('log('), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
 
 
 
 
-btn4 = Button(frame3, text="4", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(4)).pack(side=LEFT, expand=True, fill="both")
-btn5 = Button(frame3, text="5", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(5)).pack(side=LEFT, expand=True, fill="both")
-btn6 = Button(frame3, text="6", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(6)).pack(side=LEFT, expand=True, fill="both")
-btn_sub = Button(frame3, text="-", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('-')).pack(side=LEFT, expand=True, fill="both")
-btn_cos = Button(frame3, text="cos", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('cos(')).pack(side=LEFT, expand=True, fill="both")
-btn_ln = Button(frame3, text="ln", font="Verdana 22", bd=0, relief=GROOVE, command=lambda : btn_clicked('ln(')).pack(side=LEFT, expand=True, fill="both")
+btn4 = Button(frame3, text="4", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(4), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn5 = Button(frame3, text="5", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(5), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn6 = Button(frame3, text="6", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(6), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_sub = Button(frame3, text="-", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('-'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_cos = Button(frame3, text="cos", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('cos('), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_ln = Button(frame3, text="ln", font="Verdana 22", bd=0, relief=GROOVE, command=lambda : btn_clicked('ln('), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
 
 
-btn1 = Button(frame4, text="1", font="Verdana 22", bd=0, relief=GROOVE,  command=lambda :btn_clicked(1)).pack(side=LEFT, expand=True, fill="both")
-btn2 = Button(frame4, text="2", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(2)).pack(side=LEFT, expand=True, fill="both")
-btn3 = Button(frame4, text="3", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(3)).pack(side=LEFT, expand=True, fill="both")
-btn_divide = Button(frame4, text="/", font="Verdana 22", bd=0, relief=GROOVE, command=lambda : btn_clicked('/')).pack(side=LEFT, expand=True, fill="both")
-btn_tan = Button(frame4, text="tan", font="Verdana 22", bd=0, relief=GROOVE, command=lambda : btn_clicked('tan(')).pack(side=LEFT, expand=True, fill="both")
-btn_pow = Button(frame4, text="^", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('**')).pack(side=LEFT, expand=True, fill="both")
+btn1 = Button(frame4, text="1", font="Verdana 22", bd=0, relief=GROOVE,  command=lambda :btn_clicked(1), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn2 = Button(frame4, text="2", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(2), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn3 = Button(frame4, text="3", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(3), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_divide = Button(frame4, text="/", font="Verdana 22", bd=0, relief=GROOVE, command=lambda : btn_clicked('/'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_tan = Button(frame4, text="tan", font="Verdana 22", bd=0, relief=GROOVE, command=lambda : btn_clicked('tan('), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_pow = Button(frame4, text="^", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('**'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
 
 
-btn_decimal = Button(frame5, text=".", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('.')).pack(side=LEFT, expand=True, fill="both")
-btn0 = Button(frame5, text="0", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(0)).pack(side=LEFT, expand=True, fill="both")
-btn_clear = Button(frame5, text="C",font="Verdana 22", bd=0, relief=GROOVE,command=btn_clear).pack(side=LEFT,expand=True,fill="both")
-btn_add = Button(frame5, text="+", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('+')).pack(side=LEFT, expand=True, fill="both")
-btn_square =  Button(frame5, text='x\u00b2', font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('**2')).pack(side=LEFT, expand=True, fill="both")
-btn_root =  Button(frame5, text='\u221A', font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('\u221A')).pack(side=LEFT, expand=True, fill="both")
+btn_decimal = Button(frame5, text=".", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('.'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn0 = Button(frame5, text="0", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(0), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_clear = Button(frame5, text="C",font="Verdana 22", bd=0, relief=GROOVE,command=btn_clear, activebackground="LIGHT GREY").pack(side=LEFT,expand=True,fill="both")
+btn_add = Button(frame5, text="+", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('+'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_square =  Button(frame5, text='x\u00b2', font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('**2'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_root =  Button(frame5, text='\u221A', font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('\u221A'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
 
-btn_pi = Button(frame6, text=PI, font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(math.pi)).pack(side=LEFT, expand=True, fill="both")
-btn_open_bracket = Button(frame6, text="(", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('(')).pack(side=LEFT, expand=True, fill="both")
-btn_closed_bracket = Button(frame6, text=")", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(')')).pack(side=LEFT, expand=True, fill="both")
-btn_e = Button(frame6, text="e", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(math.e)).pack(side=LEFT, expand=True, fill="both")
-btn_fact = Button(frame6, text="x!", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('!')).pack(side=LEFT, expand=True, fill="both")
-btn_equal = Button(frame6, text= "=", font="Verdana 22", bd=0, relief=GROOVE, command=result).pack(side=LEFT, expand=True, fill="both")
+btn_pi = Button(frame6, text=PI, font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(math.pi), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_open_bracket = Button(frame6, text="(", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('('), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_closed_bracket = Button(frame6, text=")", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(')'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_e = Button(frame6, text="e", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked(math.e), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_fact = Button(frame6, text="x!", font="Verdana 22", bd=0, relief=GROOVE, command=lambda :btn_clicked('!'), activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
+btn_equal = Button(frame6, text= "=", font="Verdana 22", bd=0, relief=GROOVE, command=result, activebackground="LIGHT GREY").pack(side=LEFT, expand=True, fill="both")
 
 
 root.mainloop()
